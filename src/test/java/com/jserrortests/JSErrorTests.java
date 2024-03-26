f77339ec0f8de24912fea8d86e846f2692360da8
nouha-besbes <nouhabesbes@yahoo.fr>
2024-03-26 18:02:39 +0100

    add js error test

---
+++ src/test/java/com/jserrortests/JSErrorTests.java
@@ -0,0 +1,35 @@
+package com.jserrortests;
+
+import java.util.List;
+
+import org.openqa.selenium.logging.LogEntry;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import com.base.TestUtilities;
+import com.pages.JSErrorPage;
+
+public class JSErrorTests extends TestUtilities {
+
+	@Test
+	public void jsErrorTest() {
+		log.info("Starting jsErrorTest");
+		SoftAssert softAssert = new SoftAssert();
+
+		// Open JSErrorPage
+		JSErrorPage jSErrorPage = new JSErrorPage(driver, log);
+		jSErrorPage.openPage();
+
+		// Get logs
+		List<LogEntry> logs = getBrowserLogs();
+
+		// Verifying there are no JavaScript errors in console
+		for (LogEntry logEntry : logs) {
+			if (logEntry.getLevel().toString().equals("SEVERE")) {
+				softAssert.fail("Severe error: " + logEntry.getMessage());
+			}
+		}
+		softAssert.assertAll();
+	}
+
+}
